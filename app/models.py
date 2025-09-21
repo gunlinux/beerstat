4863791ddb5154e44010d90ff69b1bba8868a948
loki <gunlinux@ya.ru>
2025-09-22 05:40:33 +1000

    prod ready

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from app.extensions import db
 
 
 class BeerDonation(db.Model):
+    __tablename__ = 'donations'
     id = Column(Integer, primary_key=True)
     name = Column(String(30))
     date = Column(DateTime, default=datetime.now)
